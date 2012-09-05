089d45152427422f93c6238873e7ced0d377067e
yasin <iskorum@hotmail.com>
2012-09-05 11:01:24 +0300

    Template add and Fix

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
+	url(r'^$', TemplateView.as_view(template_name="main.html")),
 	# Examples:
 	# url(r'^$', 'project_name.views.home', name='home'),
 	# url(r'^project_name/', include('project_name.foo.urls')),
