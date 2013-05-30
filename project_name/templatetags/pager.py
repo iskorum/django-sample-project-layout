731a230d74993d27c2f8f6c3c093e37f83d962b1
yasin <iskorum@hotmail.com>
2013-05-30 13:35:15 +0300

    pager.py edits

--- project_name/templatetags/pager.py
+++ project_name/templatetags/pager.py
@@ -4,11 +4,12 @@ import re, json
 
 register = template.Library()
 
+
 @register.tag
-def pager(parser,token):
+def pager(parser, token):
 
 	try:
-#		tag_name, obj = token.split_contents()
+		#tag_name, obj = token.split_contents()
 		tag_name, arg = token.contents.split(None, 1)
 	except ValueError:
 		raise template.TemplateSyntaxError("%r tag requires exactly two argument" % token.contents.split()[0])
@@ -18,26 +19,26 @@ def pager(parser,token):
 		if not m:
 			raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
 		obj, var_name = m.groups()
-	
-	#	if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-	#		raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
+
+		#if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+		#	raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
 	except:
 		obj = arg
 		var_name = None
-		
+
 	return PagerNode(obj,var_name)
+
 
 class PagerNode(template.Node):
 	def __init__(self, obj, var_name):
 
 		self.template_file = 'widgets/pagination.html'
 		self.spacer = 0
-		
+
 		self.pager_var = 'sayfa'
 		self.var_name = var_name
 		self.obj = obj
 
-	
 	def render(self, context):
 		try:
 			current_page = context[self.obj].number
@@ -46,7 +47,7 @@ class PagerNode(template.Node):
 			items = []
 			if total_page == 1:
 				pass
-			
+
 			elif total_page < 8:
 				for i in range(1,total_page+1):
 					items.append(i)
@@ -73,7 +74,7 @@ class PagerNode(template.Node):
 					items.append(self.spacer)
 					for i in range(total_page-1,total_page+1):
 						items.append(i)
-			
+
 			p = {
 				'current_page':current_page,
 				'total_pages':total_page,
@@ -97,5 +98,5 @@ class PagerNode(template.Node):
 				return t.render(template.Context({'pager': p, }, autoescape=context.autoescape))
 		except KeyError:
 			return ''
-#	t = template.loader.get_template('small_fragment.html')
-#    return t.render(Context({'var': obj}, autoescape=context.autoescape))
+#t = template.loader.get_template('small_fragment.html')
+#return t.render(Context({'var': obj}, autoescape=context.autoescape))
