95a7d2fd3a07f7c0fadb130f8374fc6e0f51911b
Yasin YILMAZ <iskorum@hotmail.com>
2015-05-19 14:18:49 +0300

    paginator edited

--- project_name/templatetags/pager.py
+++ project_name/templatetags/pager.py
@@ -9,94 +9,87 @@ register = template.Library()
 def pager(parser, token):
 
 	try:
-		#tag_name, obj = token.split_contents()
 		tag_name, arg = token.contents.split(None, 1)
 	except ValueError:
 		raise template.TemplateSyntaxError("%r tag requires exactly two argument" % token.contents.split()[0])
 
 	try:
 		m = re.search(r'(.*?) as (\w+)', arg)
-		if not m:
-			raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
-		obj, var_name = m.groups()
-
-		#if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-		#	raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
+		if not m: raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
+		object_name, var_name = m.groups()
 	except:
-		obj = arg
-		var_name = None
+		object_name, var_name = arg, None
 
-	return PagerNode(obj,var_name)
+	return PagerNode(object_name, var_name)
 
 
 class PagerNode(template.Node):
-	def __init__(self, obj, var_name):
 
-		self.template_file = 'widgets/pagination.html'
+	def __init__(self, object_name, var_name):
+
+		self.template_file = 'utils/pagination.html'
 		self.spacer = 0
 
 		self.pager_var = 'sayfa'
 		self.var_name = var_name
-		self.obj = obj
+		self.object_name = object_name
 
 	def render(self, context):
 		try:
-			current_page = context[self.obj].number
-			total_page = context[self.obj].paginator.num_pages
+			pager = context[self.object_name]
 
-			items = []
-			if total_page == 1:
-				pass
+			current_page = pager.number
+			total_pages = pager.paginator.num_pages
 
-			elif total_page < 8:
-				for i in range(1,total_page+1):
-					items.append(i)
+			# ------------------------- page numbers -----------------------------
+			pages = []
+			if total_pages == 1: pass
+			elif total_pages < 8: map(pages.append, range(1, total_pages+1))
 			else:
 				if current_page <= 4:
-					for i in range(1,current_page+2):
-						items.append(i)
-					items.append(self.spacer)
-					for i in range(total_page-1,total_page+1):
-						items.append(i)
-				elif current_page >= total_page-3:
-					for i in range(1,3):
-						items.append(i)
-					items.append(self.spacer)
-					for i in range(current_page-1,total_page+1):
-						items.append(i)
+					map(pages.append, range(1, current_page+2))
+					pages.append(self.spacer)
+					map(pages.append, range(total_pages-1, total_pages+1))
+				elif current_page >= total_pages-3:
+					map(pages.append, range(1, 3))
+					pages.append(self.spacer)
+					map(pages.append, range(current_page-1, total_pages+1))
 				else:
-					for i in range(1,3):
-						items.append(i)
-					items.append(self.spacer)
-					items.append(current_page-1)
-					items.append(current_page)
-					items.append(current_page+1)
-					items.append(self.spacer)
-					for i in range(total_page-1,total_page+1):
-						items.append(i)
+					map(pages.append, range(1, 3))
+					pages.append(self.spacer)
+					pages.append(current_page-1)
+					pages.append(current_page)
+					pages.append(current_page+1)
+					pages.append(self.spacer)
+					map(pages.append, range(total_pages-1, total_pages+1))
+			# --------------------------------------------------------------------
+
+			# ------------------------- get parameters ---------------------------
+			data = context['request'].GET.copy()
+			get_params = ""
+			try: data.pop(self.pager_var)
+			except: pass
+			for key, value in data.iteritems(): get_params += "&{0}={1}".format(key, value)
+			# --------------------------------------------------------------------
 
 			p = {
-				'current_page':current_page,
-				'total_pages':total_page,
+				'total_pages': total_pages,
 				'previous_page': current_page-1,
+				'current_page': current_page,
 				'next_page': current_page+1,
-				'has_previous': context[self.obj].has_previous(),
-				'has_next': context[self.obj].has_next(),
-				'items': items
-				}
+				'has_previous': pager.has_previous(),
+				'has_next': pager.has_next(),
+				'pages': pages,
+				'pager_var': self.pager_var,
+				'get_params': get_params
+			}
+
 			if self.var_name:
 				context[self.var_name] = json.dumps(p)
 				return ''
 			else:
-				data = context['request'].GET.copy()
-				try:
-					data.pop(self.pager_var)
-				except:
-					pass
-				p['data'] = data
 				t = template.loader.get_template(self.template_file)
-				return t.render(template.Context({'pager': p, }, autoescape=context.autoescape))
-		except KeyError:
-			return ''
-#t = template.loader.get_template('small_fragment.html')
-#return t.render(Context({'var': obj}, autoescape=context.autoescape))
+				return t.render(template.Context({'pager': p}, autoescape=context.autoescape))
+
+		except Exception, e:
+			print e
