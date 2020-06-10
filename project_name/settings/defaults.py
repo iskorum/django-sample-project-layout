15d62a01fa3a380ea175f25b042f2a1ca74bc753
Yasin YILMAZ <yasin@unicrow.com>
2020-06-10 12:32:58 +0300

    some updates

--- project_name/settings/defaults.py
+++ project_name/settings/defaults.py
@@ -122,7 +122,7 @@ MEDIA_URL = "/media/"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Static files (CSS, JavaScript, Images)
